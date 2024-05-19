2ef956a658c529e14ee6415a91e3ca4a17739dca
Kaushal <kaushal.deokar@gmail.com>
2024-05-20 00:18:42 +0530

    refractor

--- src/MySQLConnection.java
+++ src/MySQLConnection.java
@@ -1,7 +1,7 @@
 import java.sql.*;
 
 
-public class Main {
+public class MySQLConnection {
 
     // final - makes string variable immutable / cannot be overridden
     private static final String Url= "jdbc:mysql://localhost:3306/jdbc_crud";
